2292860c9baff281a42cf19bf2370781ad33fcfd
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-07-02 16:13:56 +0200

    add design system in css

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,14 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
 
+import DesignSystem from "./pages/DesignSystem"
+
+import "./styles/reset.css"
+import "./styles/index.css"
+
 // biome-ignore lint: non-null-assertion
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <div />
+    <DesignSystem />
   </React.StrictMode>,
 )
---
+++ src/pages/DesignSystem.tsx
@@ -0,0 +1,42 @@
+function DesignSystem() {
+  return (
+    <main>
+      <div className="header --xl">dolor sit</div>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Commodi quod
+        aut dicta porro quos, et consectetur natus divrum. Corporis officia
+        optio cumque laboriosam odit, impedit esse neque dolorum, laudantium,
+        reiciendis dolor commodi. Quo quos sint officia, voluptates debitis
+        facilis sit possimus, voluptatem, ad placeat alias soluta accusantium
+        temporibus quibusdam quia.
+      </p>
+      <div className="header --l">
+        Lorem ipsum dolor <br />
+        sit amet
+      </div>
+      <div className="header --m">
+        Lorem ipsum dolor sit <br />
+        amet consectetuer adipiscing
+      </div>
+      <div className="header --s">
+        Lorem ipsum dolor sit amet consectetuer adipiscing
+      </div>
+      <div className="header --xs">
+        Lorem ipsum dolor sit amet consectetuer adipiscing
+      </div>
+
+      <div className="flex">
+        <button className="button" type="button" disabled>
+          Button
+        </button>
+        <button className="button" type="button">
+          Button
+        </button>
+      </div>
+      <div className="sub-card">option</div>
+      <div className="sub-card --active">option</div>
+    </main>
+  )
+}
+
+export default DesignSystem
