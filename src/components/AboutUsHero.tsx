224594d8cdde548d9a264da012f6e7dab4d35148
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-07-10 10:12:24 +0200

    add commitment section

--- src/components/AboutUsHero.tsx
+++ src/components/AboutUsHero.tsx
@@ -3,7 +3,7 @@ import Container from "./Container"
 const AboutUsHero = () => {
   return (
     <Container>
-      <section className="bg-aboutUs-img-mobile sm:bg-aboutUs-img-tablet lg:bg-aboutUs-img-desktop rounded-xl bg-cover bg-center bg-no-repeat pb-20 pt-28 leading-8 sm:py-28 lg:py-36">
+      <section className="bg-aboutUs-img-mobile sm:bg-aboutUs-img-tablet lg:bg-aboutUs-img-desktop mb-32 rounded-xl bg-cover bg-center bg-no-repeat pb-20 pt-28 leading-8 sm:mb-36 sm:py-28 lg:mb-40 lg:py-36">
         <div className="px-8 text-neutral-white sm:px-14">
           <h1 className="mb-8 text-center font-serif text-3xl font-bold leading-10 sm:w-2/3 sm:text-left sm:text-[3rem] lg:w-2/3 lg:text-[4.5rem] lg:leading-[4rem]">
             About Us
--- src/pages/AboutUsPage.tsx
+++ src/pages/AboutUsPage.tsx
@@ -1,9 +1,11 @@
 import AboutUsHero from "../components/AboutUsHero"
+import Commitment from "../components/Commitment"
 
 const AboutUsPage = () => {
   return (
     <main>
       <AboutUsHero />
+      <Commitment />
     </main>
   )
 }
