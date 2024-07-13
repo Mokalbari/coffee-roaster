8c771f2e1edaaa0a81b3afec7f67d48854b64d00
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-07-13 12:08:31 +0200

    refacto FormContext with a userChoice state object
    - add comments to clarify the steps

--- src/context/FormContext.tsx
+++ src/context/FormContext.tsx
@@ -1,6 +1,20 @@
 import { createContext, useState } from "react"
+import type coffeeSubscriptionData from "../lib/coffeeSubscriptionData"
 import type { ReactNode } from "react"
+import type { CoffeeStepsName } from "../lib/coffeeSubscriptionData"
 
+// This part is creating a mapped type. It iterates over each key K in the union type CoffeeStepsName.
+// CoffeeStepsName is a union type of string literals:
+// "brewing" | "beanTypes" | "quantities" | "grindOptions" | "deliveryOptions".
+
+type SubscriptionEntries = {
+  [K in CoffeeStepsName]: (typeof coffeeSubscriptionData)[K][number]["type"]
+}[CoffeeStepsName]
+
+// The user choice can be both an empty string or a key in the SubcriptionEntry
+type ValidSubscriptionEntries = SubscriptionEntries | ""
+
+// This is defining Props and Type for the context
 type Props = {
   children: ReactNode
 }
@@ -8,39 +22,41 @@ type Props = {
 type Type = {
   isToggled: boolean
   setIsToggled: React.Dispatch<React.SetStateAction<boolean>>
-  isCapsuleSelected: boolean
-  setIsCapsuleSelected: React.Dispatch<React.SetStateAction<boolean>>
-  userChoice: string[]
-  setUserChoice: React.Dispatch<React.SetStateAction<never[]>>
+  userSelection: Record<CoffeeStepsName, ValidSubscriptionEntries>
+  setUserSelection: React.Dispatch<
+    React.SetStateAction<Record<CoffeeStepsName, string>>
+  >
 }
 
+// Default values that are passed to the context down below.
 const defaultFormContext: Type = {
   isToggled: false,
   setIsToggled: () => {},
-  isCapsuleSelected: false,
-  setIsCapsuleSelected: () => {},
-  userChoice: [],
-  setUserChoice: () => {},
+  userSelection: {
+    brewing: "",
+    beanTypes: "",
+    quantities: "",
+    grindOptions: "",
+    deliveryOptions: "",
+  },
+  setUserSelection: () => {},
 }
-
 export const FormContext = createContext(defaultFormContext)
 
+// This initialize states and creates a Provider
 export const FormContextProvider = ({ children }: Props) => {
   const [isToggled, setIsToggled] = useState(defaultFormContext.isToggled)
-  const [isCapsuleSelected, setIsCapsuleSelected] = useState(
-    defaultFormContext.isCapsuleSelected,
+  const [userSelection, setUserSelection] = useState(
+    defaultFormContext.userSelection,
   )
-  const [userChoice, setUserChoice] = useState([])
 
   return (
     <FormContext.Provider
       value={{
         isToggled,
         setIsToggled,
-        isCapsuleSelected,
-        setIsCapsuleSelected,
-        userChoice,
-        setUserChoice,
+        userSelection,
+        setUserSelection,
       }}
     >
       {children}
