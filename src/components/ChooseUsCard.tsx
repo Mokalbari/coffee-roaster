d53258b9e078a01d79827dd67376a579d508fd5c
Mokalbari <150693283+Mokalbari@users.noreply.github.com>
2024-07-16 16:44:25 +0200

    add animation on home page coffee selection + choose us

--- src/components/ChooseUsCard.tsx
+++ src/components/ChooseUsCard.tsx
@@ -1,19 +1,26 @@
+import type { Variants } from "framer-motion"
+import { motion } from "framer-motion"
+
 interface Props {
   key: number
   imgSrc: string
   title: string
   textContent: string
+  variants: Variants
 }
 
-const ChooseUsCard = ({ imgSrc, title, textContent }: Props) => {
+const ChooseUsCard = ({ imgSrc, title, textContent, variants }: Props) => {
   return (
-    <div className="mx-auto flex max-w-4/5 flex-col items-center rounded-xl bg-accent-primary px-8 py-16 text-center sm:py-8 sm:max-lg:flex-row sm:max-lg:gap-16 sm:max-lg:text-left lg:max-w-sm lg:p-16">
+    <motion.li
+      variants={variants}
+      className="mx-auto flex max-w-4/5 flex-col items-center rounded-xl bg-accent-primary px-8 py-16 text-center sm:py-8 sm:max-lg:flex-row sm:max-lg:gap-16 sm:max-lg:text-left lg:max-w-sm lg:p-16"
+    >
       <img src={imgSrc} alt={title} className="my-auto mb-8 sm:max-lg:my-0" />
       <div className="card-container__text-container">
         <h3 className="mb-4 font-serif text-2xl font-semibold">{title}</h3>
         <p className="lg:max-w-60">{textContent}</p>
       </div>
-    </div>
+    </motion.li>
   )
 }
 
--- src/components/CoffeeSelection.tsx
+++ src/components/CoffeeSelection.tsx
@@ -2,30 +2,59 @@ import CoffeeCard from "./CoffeeCard"
 import Container from "./Container"
 import { coffeeSelection } from "../lib/mappingArray"
 import "../styles/CoffeeSelection.css"
+import type { Variants } from "framer-motion"
 import { motion } from "framer-motion"
 
 const CoffeeSelection = () => {
+  const list: Variants = {
+    visible: {
+      opacity: 1,
+      transition: {
+        when: "beforeChildren",
+        staggerChildren: 0.3,
+      },
+    },
+    hidden: {
+      opacity: 0,
+      transition: {
+        when: "afterChildren",
+      },
+    },
+  }
+
+  const item: Variants = {
+    visible: { opacity: 1, x: 0 },
+    hidden: { opacity: 0, x: -100 },
+  }
+
   return (
     <Container>
       <motion.h2
         initial={{ opacity: 0, y: 100 }}
-        viewport={{ amount: "all", once: true }}
+        viewport={{ margin: "-200px", once: true }}
         whileInView={{ opacity: 1, y: 0 }}
         transition={{ duration: 0.5, delay: 0.2 }}
         className="text-gradient text-center font-serif text-[40px] font-extrabold sm:text-[96px] sm:max-md:text-[80px] lg:text-[150px]"
       >
         Our collection
       </motion.h2>
-      <ul className="flex flex-col flex-wrap gap-8 sm:-translate-y-10 lg:-translate-y-20 lg:flex-row lg:justify-center">
-        {coffeeSelection.map(item => (
+      <motion.ul
+        viewport={{ margin: "-200px", once: true }}
+        variants={list}
+        initial="hidden"
+        whileInView="visible"
+        className="flex flex-col flex-wrap gap-8 sm:-translate-y-10 lg:-translate-y-20 lg:flex-row lg:justify-center"
+      >
+        {coffeeSelection.map(coffee => (
           <CoffeeCard
-            key={item.id}
-            imgSrc={item.image}
-            title={item.name}
-            text={item.description}
+            key={coffee.id}
+            imgSrc={coffee.image}
+            title={coffee.name}
+            text={coffee.description}
+            variants={item} // Pass the item variant here
           />
         ))}
-      </ul>
+      </motion.ul>
     </Container>
   )
 }
